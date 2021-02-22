4c7a4ce2db529e66549ddc8079d10652ffd2ace2
Jagadamba Sahani <51432196+Jagadambass@users.noreply.github.com>
2021-02-23 02:30:51 +0530

    Create GAT.py

---
+++ examples/Graph_Neural_Networks/DGL-PyTorch/GAT.py
@@ -0,0 +1,24 @@
+import torch
+import dgl
+import graphgallery
+
+print("GraphGallery version: ", graphgallery.__version__)
+print("Torch version: ", torch.__version__)
+print("DGL version: ", dgl.__version__)
+
+'''
+Load Datasets
+- cora/citeseer/pubmed
+'''
+from graphgallery.datasets import Planetoid
+data = Planetoid('cora', root="~/GraphData/datasets/", verbose=False)
+graph = data.graph
+splits = data.split_nodes()
+
+graphgallery.set_backend("dgl")
+
+from graphgallery.gallery import GAT
+trainer = GAT(graph, device="gpu", seed=123).process(attr_transform="normalize_attr").build()
+his = trainer.train(splits.train_nodes, splits.val_nodes, verbose=1, epochs=200)
+results = trainer.test(splits.test_nodes)
+print(f'Test loss {results.loss:.5}, Test accuracy {results.accuracy:.2%}')
